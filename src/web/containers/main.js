9d911c96b498deb7e2abea87187110e4a3b1e195
hsien wei <hsien.w.wei@gmail.com>
2023-03-23 13:07:48 +0800

    update plugin to create json source

--- src/web/containers/main.js
+++ src/web/containers/main.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Main = () => {
     return (
         <div>
-            <h2>React Offline Catch Plugin Demo</h2>
+            <h2>React Offline Catch Plugin Demo123</h2>
         </div>
     );
 };
